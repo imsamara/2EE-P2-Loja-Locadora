710a220ab9562c14dfcb8a23a8aba860831bbf2b
EricFelix <ericfelixfelixpinto@gmail.com>
2026-06-24 21:54:20 -0300

    feat: finalizado a parte de categoria

---
+++ src/main/java/business/interfaces/IGerenciamentoCategoria.java
@@ -0,0 +1,21 @@
+package business.interfaces;
+ 
+import java.util.List;
+ 
+import entidades.Categoria;
+ 
+public interface IGerenciamentoCategoria {
+ 
+    public abstract boolean cadastrarCategoria(Categoria categoria);
+ 
+    public abstract Categoria buscarCategoria(int id);
+ 
+    public abstract List<Categoria> listarCategorias();
+ 
+    public abstract boolean atualizarCategoria(int id, String nome, String descricao);
+ 
+    public abstract boolean desativarCategoria(int id);
+ 
+    public abstract int gerarProximoId();
+ 
+}
